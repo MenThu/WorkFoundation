d758a26e9f2012fb69d892c6a26c2152fd8a50c6
MenThu <422729946@qq.com>
2016-07-21 09:28:49 +0800

    ADD

---
+++ WorkFoundation/WorkFoundation.h
@@ -0,0 +1,26 @@
+//
+//  WorkFoundation.h
+//  TestGitHub
+//
+//  Created by MenThu on 16/7/21.
+//  Copyright © 2016年 官辉. All rights reserved.
+//
+
+#ifndef WorkFoundation_h
+#define WorkFoundation_h
+
+
+#if __has_include(<WorkFoundation/WorkFoundation.h>)
+
+
+#define MenThuDebug 1
+
+#else
+
+#define MenThuDebug 0
+
+
+#endif
+
+
+#endif /* WorkFoundation_h */
