6ce66bb4cf428b78b2c9a9a1e715fb001bbb5080
MenThu <422729946@qq.com>
2016-07-23 14:35:36 +0800

    1

---
+++ WorkFoundation/MyCategory/NSDictionary+Convinience.m
@@ -0,0 +1,24 @@
+//
+//  NSDictionary+Convinience.m
+//  CGLearn
+//
+//  Created by MenThu on 16/7/18.
+//  Copyright © 2016年 官辉. All rights reserved.
+//
+
+#import "NSDictionary+Convinience.h"
+
+@implementation NSDictionary (Convinience)
+
+- (BOOL)haveKey:(NSString*)keyString
+{
+    NSArray *keyArray = [self allKeys];
+    for (NSString *subKeyString in keyArray) {
+        if ([subKeyString isEqualToString:keyString]) {
+            return YES;
+        }
+    }
+    return NO;
+}
+
+@end
